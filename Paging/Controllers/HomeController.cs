1f156adafeb48b03c43249b3e1547f0fa423a074
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-04-09 23:18:15 +0300

    Add project files.

---
+++ Paging/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Paging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paging.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int id=1)
+        {
+            var pageInfo = new PageInfo { PageNumber = id, PageSize = 10, TotalItems = 103 };
+            return View(pageInfo);
+        }
+    }
+}
---
+++ Paging/Helpers/PageHelpers.cs
@@ -0,0 +1,59 @@
+﻿using Paging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paging.Helpers
+{
+    public static class PageHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper helper, PageInfo pageInfo, Func<int, string> url)
+        {
+            StringBuilder result = new StringBuilder();
+
+       
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", url(1));
+                tag.InnerHtml = "First";
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            
+
+            for (int i = pageInfo.PageNumber-2; i <= pageInfo.PageNumber+2; i++)
+            {
+                if(i>0 && i<= pageInfo.TotalPages)
+                {
+                     tag = new TagBuilder("a");
+                    tag.MergeAttribute("href", url(i));
+                    tag.InnerHtml = i.ToString();
+
+                    if (i == pageInfo.PageNumber)
+                    {
+                        tag.AddCssClass("selected");
+                        tag.AddCssClass("btn btn-primary");
+                       
+                    }
+                    else
+                    {
+                        tag.AddCssClass("btn btn-default");
+                    }
+                    result.Append(tag.ToString());
+                }
+            }
+
+            tag = new TagBuilder("a");
+            tag.MergeAttribute("href", url(pageInfo.TotalPages));
+            tag.InnerHtml = "Last";
+            tag.AddCssClass("btn btn-default");
+            result.Append(tag.ToString());
+
+
+
+
+            return new MvcHtmlString(result.ToString());
+        }
+    }
+}
